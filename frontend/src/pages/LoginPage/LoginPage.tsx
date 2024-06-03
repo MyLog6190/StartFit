304930f0361016e8a4d13e3156661ac772c386af
mylog6190 <adg6190@naver.com>
2024-06-03 15:25:01 +0900

    commit

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -2,7 +2,6 @@ import { SNS_SIGN_IN_DOMAIN } from "@/api/auth";
 import LoginForm from "@/components/login/LoginFrom";
 import kakaoLoginImage from "@asset/png/login-btn/kakao_login_button.png";
 import naverLoginImage from "@asset/png/login-btn/naver_login_button.png";
-import googleLoginImage from "@asset/png/login-btn/google_login_button.png";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LoginBtn, LoginContainer, Logo, Wrapper } from "./LoginPage.style";
@@ -29,9 +28,6 @@ function LoginPage() {
         </Link>
         <Link to={SNS_SIGN_IN_DOMAIN("kakao")}>
           <LoginBtn $image={kakaoLoginImage} />
-        </Link>
-        <Link to={SNS_SIGN_IN_DOMAIN("google")}>
-          <LoginBtn $image={googleLoginImage} />
         </Link>
       </LoginContainer>
     </Wrapper>
